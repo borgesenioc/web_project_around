bf5a4251a92c124935ae7eeb0737e313ce0d7351
Enio Borges <borgesenioc@gmail.com>
2025-03-26 08:12:41 -0300

    feat: complete the feature that opens and closes the popup adding and removing new cards

--- scripts/popup-card.js
+++ scripts/popup-card.js
@@ -2,29 +2,23 @@ let addCardButton = document.querySelector('.profile__add-button')
 let popupCardElement = document.querySelector('.popup-card')
 let closeCardButton = document.querySelector('.popup-card__close')
 
-// Definidr a funcao que adiciona o modificador .popup-open de popup
 function addPopupOpenModifier() {
-    popupElement.classList.add('popup_opened')
+    popupCardElement.classList.add('popup_opened')
 }
-
-// Definir a funcao que remove o modificador .popup-open de popup
-
 function removePopupOpenModifier() {
-    popupElement.classList.remove('popup_opened')
+    popupCardElement.classList.remove('popup_opened')
 }
 
-// Adicionar event listener no clique do botao editar
-editButton.addEventListener('click', addPopupOpenModifier)
+addCardButton.addEventListener('click', addPopupOpenModifier)
 
-// Adicionar event listener no clique do botao fechar popup
-closeButton.addEventListener('click', removePopupOpenModifier)
+closeCardButton.addEventListener('click', removePopupOpenModifier)
 
 export function openPopup() {
-    const popup = document.querySelector('.popup')
+    const popup = document.querySelector('.popup-card')
     popup.classList.add('popup-opened')
 }
 
 export function closePopup() {
-    const popup = document.querySelector('.popup')
+    const popup = document.querySelector('.popup-card')
     popup.classList.remove('popup-opened')
 }
