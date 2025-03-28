5ad5870dab12d37baff1bc09bf56c42cd087f9e2
Enio Borges <borgesenioc@gmail.com>
2025-03-28 09:35:27 -0300

    feat: create listener that creates a new image highlight on click

--- scripts/cardInteractions.js
+++ scripts/cardInteractions.js
@@ -2,7 +2,9 @@ import { elementsContainer } from './loadInitialCards.js'
 
 let deleteButtons = document.querySelectorAll('.elements__card-trash-button')
 
-let clickableImages = document.querySelectorAll('.elements__card-image')
+let clickableImages = document.querySelectorAll('.elements__card-image-button')
+
+let imageTemplate = document.querySelector('#image-highlight').content
 
 // method that toggles a css modifier on the like button
 function toggleLikeButton(evt) {
@@ -15,6 +17,21 @@ const deleteCard = (evt) => {
 }
 
 // method that creates an image popup on click
+const createImageHighlight = (evt) => {
+    let imageHighlight = document.importNode(imageTemplate, true) // creates the image popup from the image highlight template
+    let highlightImage = imageHighlight.querySelector('.popup-image__highlight')
+    highlightImage.src = evt.target.parentElement.querySelector(
+        '.elements__card-image'
+    ).src
+
+    let highlightParagraph = imageHighlight.querySelector(
+        '.popup-image__paragraph'
+    )
+    highlightParagraph.textContent = evt.target.parentElement.querySelector(
+        '.elements__card-header-title'
+    ).textContent
+    document.body.appendChild(imageHighlight)
+}
 
 // method that deletes the image popup on click
 
@@ -32,7 +49,14 @@ deleteButtons.forEach((button) => {
     })
 })
 
-// make the image clickable
+// a listener for each image
+clickableImages.forEach((button) => {
+    button.addEventListener('click', (evt) => {
+        createImageHighlight(evt)
+    })
+})
+
+// make the image clickable DONE
 
 // create a popup template with the whole image DONE
 
